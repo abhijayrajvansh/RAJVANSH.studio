5c229b3f8ab618de721b8f5eee9465489a54faf5
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2023-12-30 22:52:53 +0530

    update: added gsap mouse enter-leave animation

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const page1Content = document.getElementById("page1Content");
 const cursor = document.getElementById('cursor');
 
+page1Content.addEventListener('mouseenter', (scale) => {
+    gsap.to(cursor, {
+        scale:1
+    })
+})
+
 page1Content.addEventListener('mousemove', (mousePointerCordinates) => {
     gsap.to(cursor, {
         x:mousePointerCordinates.x, y: mousePointerCordinates.y
+    })
+})
+
+page1Content.addEventListener('mouseleave', (scale) => {
+    gsap.to(cursor, {
+        scale:0
     })
 })
