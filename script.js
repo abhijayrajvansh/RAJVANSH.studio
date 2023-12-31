81fa690647d5b3dac2a3eef9e2a4a1b60eeb7510
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2023-12-31 18:23:58 +0530

    update: gsap animation for mouse

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const page1Content = document.getElementById("page1Content");
-const cursor = document.getElementById('cursor');
+const cusrsorEffect = () => {
+    const page1Content = document.getElementById("page1Content");
+    const cursor = document.getElementById('cursor');
 
-page1Content.addEventListener('mouseenter', (scale) => {
-    gsap.to(cursor, {
-        scale:1
+    page1Content.addEventListener('mouseenter', () => {
+        gsap.to(cursor, {
+            scale:1
+        })
     })
-})
 
-page1Content.addEventListener('mousemove', (mousePointerCordinates) => {
-    gsap.to(cursor, {
-        x:mousePointerCordinates.x, y: mousePointerCordinates.y
+    page1Content.addEventListener('mousemove', (mousePointerCordinates) => {
+        gsap.to(cursor, {
+            x:mousePointerCordinates.x, y: mousePointerCordinates.y
+        })
     })
-})
 
-page1Content.addEventListener('mouseleave', (scale) => {
-    gsap.to(cursor, {
-        scale:0
+    page1Content.addEventListener('mouseleave', () => {
+        gsap.to(cursor, {
+            scale:0
+        })
     })
-})
+}
+
+cusrsorEffect()
